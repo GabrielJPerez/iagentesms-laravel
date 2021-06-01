fd27d24b5e9844ec2bb916943c583e255ad86226
Perez <gabrieljesusperez@hotmail.com>
2021-06-01 17:11:47 -0300

    Ajustou e alterou funções

--- src/gabriejperez/IagenteSms/IagenteSms.php
+++ src/gabriejperez/IagenteSms/IagenteSms.php
@@ -11,47 +11,55 @@ use Log;
  */
 class IagenteSms
 {
+    /** @var string */
+    protected $user;
+
+    /** @var string */
+    protected $pass;
+
+    /**
+     * IagenteSms constructor.
+     */
+    public function __construct()
+    {
+        $this->user = $this->getUser();
+        $this->pass = $this->getPass();
+    }
+
     /**
      * Get the IagenteSms login user
-     * Private static function
      * 
      * @return string
      */
-    private static function _getUser()
+    private function getUser()
     {
         return config('services.iagentesms.user');
     }
 
     /**
      * Get the IagenteSms login password
-     * Private static function
      * 
      * @return string
      */
-    private static function _getPass()
+    private function _getPass()
     {
-        return  config('services.iagentesms.pass');
+        return config('services.iagentesms.pass');
     }
 
     /**
      * Send the sms message to the number informed using the Iagente get api
-     * public static function
      * 
      * @param string $phone 
      * @param string $message 
      * 
      * @return array
      */
-    public static function sendMessage($phone,$message)
+    public function sendMessage($phone,$message)
     {
-        // get user and password of IagenteSms
-        $user = Self::_getUser();
-        $pass = Self::_getPass();
-
         // codifica os dados no formato de um formulário www
         $mensagem = urlencode($message.'  é o seu código de verificação da WedClub');
         // concatena a url da api com a variável carregando o conteúdo da mensagem
-        $url_api = "https://www.iagentesms.com.br/webservices/http.php?metodo=envio&usuario={$user}&senha={$pass}&celular={$phone}&mensagem={$mensagem}";
+        $url_api = "https://www.iagentesms.com.br/webservices/http.php?metodo=envio&usuario={$this->user}&senha={$this->pass}&celular={$phone}&mensagem={$mensagem}";
 
         if (app()->environment('local')) {
             $api_http = Log::debug("[{$phone}] {$mensagem}");
